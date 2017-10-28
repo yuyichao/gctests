8b7b103a6b3cb04f98745819809ac1d603f7ccc9
Yichao Yu <yyc1992@gmail.com>
2017-10-28 15:59:03 -0400

    Reorginize cpu_client, making it easier to reuse for other programs

--- cpu_client/main.cpp
+++ cpu_client/main.cpp
@@ -17,8 +17,9 @@
 #include <iostream>
 #include <sstream>
 
-static void main_loop(const char *name)
+static int get_cpu_binding(const char *name)
 {
+    // Do not close this socket, this is used for keeping track of the process from the server.
     int serverfd = socket(AF_UNIX, SOCK_STREAM, 0);
     if (serverfd == -1) {
         perror("socket");
@@ -35,27 +36,36 @@ static void main_loop(const char *name)
         perror("connect");
         exit(1);
     }
-#define ENV_PREFIX "GC_TEST_BIND_CPU="
-    const size_t prefix_len = strlen(ENV_PREFIX);
-    char buff[prefix_len + 30] = ENV_PREFIX;
-    auto real_buf = buff + prefix_len;
-    ssize_t len = recv(serverfd, real_buf, 29, 0);
-    real_buf[len] = 0;
-    if (real_buf[len - 1] != '\n') {
-        // TODO...
-        fprintf(stderr, "Wrong cpuid format: %s\n", real_buf);
+    char buff[30];
+    ssize_t len = recv(serverfd, buff, 29, 0);
+    buff[len] = 0;
+    char *endptr = buff;
+    int val = strtol(buff, &endptr, 10);
+    if (endptr == buff) {
+        fprintf(stderr, "Wrong cpuid format: %s\n", buff);
         exit(1);
     }
+    return val;
+}
+
+static void put_cpu_binding_env(int cpu)
+{
+    char buff[100];
+    sprintf(buff, "GC_TEST_BIND_CPU=%d", cpu);
     putenv(buff);
+}
+
+static int cpu_client(int argc, char *argv[])
+{
+    if (argc < 2) {
+        fprintf(stderr, "Wrong number of arguments: Usage: cpu_client socketpath command...\n");
+        exit(1);
+    }
+    put_cpu_binding_env(get_cpu_binding(argv[0]));
+    return execvp(argv[1], &argv[1]);
 }
 
 int main(int argc, char *argv[])
 {
-    if (argc <= 3) {
-        fprintf(stderr, "Wrong number of arguments: Usage: cpu_client socketpath command...\n");
-        exit(1);
-    }
-    char *path = argv[1];
-    main_loop(path);
-    return execvp(argv[2], &argv[2]);
+    return cpu_client(argc - 1, argv + 1);
 }
